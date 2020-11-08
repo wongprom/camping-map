c247db65b39df7d66fe9697fff421c7dd79be265
wongprom <>
2020-11-08 21:38:27 +0100

    WIP, add hard coded cards.

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -2,15 +2,19 @@ import React, { useState } from 'react';
 import CardLocation from '../components/CardLocation';
 import { locations } from '../data/locations';
 import {
+  Card,
   Container,
   Grid,
   GridColumn,
   GridRow,
   Header,
+  Icon,
+  Image,
   Item,
   Message,
 } from 'semantic-ui-react';
 import { Map, TileLayer } from 'react-leaflet';
+import Bild from '../data/images/tyresta.jpg';
 
 /**
  * todo - display map
@@ -20,7 +24,7 @@ const MainPage = () => {
   const [state, setstate] = useState({
     lat: 59.3212835,
     lng: 17.9754377,
-    zoom: 8,
+    zoom: 6,
   });
 
   const position = [state.lat, state.lng];
@@ -31,26 +35,73 @@ const MainPage = () => {
 
   return (
     <React.Fragment>
-      <Container fluid>
-        <Item>
-          <Header as="h1" content="My Camping Map" textAlign="center" />
-        </Item>
-        <p>bättre</p>
-        <Grid centered>
-          <GridColumn width={14}>
-            <div style={{ height: '70vh' }}>
-              <Map center={position} zoom={state.zoom}>
-                <TileLayer
-                  attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
+      <Header size="huge" content="My Camping Map" textAlign="center" />
+      <Grid centered>
+        <GridColumn width={14}>
+          <div style={{ height: '50vh' }}>
+            <Map center={position} zoom={state.zoom}>
+              <TileLayer
+                attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
 
-                {allLocations}
-              </Map>
-            </div>
-          </GridColumn>
-        </Grid>
-      </Container>
+              {allLocations}
+            </Map>
+          </div>
+        </GridColumn>
+      </Grid>
+      <Grid centered>
+        <GridRow>
+          <Header size="large" content="Som Sub header about the cards" />
+        </GridRow>
+        <GridRow>
+          <Header
+            size="medium"
+            color="grey"
+            content="Som Sub header about the cards"
+          />
+        </GridRow>
+      </Grid>
+      <Grid centered>
+        <Grid.Row columns={4}>
+          <Grid.Column>
+            <Card fluid>
+              <Image src={Bild} />
+              <Card.Content>
+                <Card.Header>Daniel</Card.Header>
+                <Card.Meta>Joined in 2016</Card.Meta>
+                <Card.Description>
+                  Daniel is a comedian living in Nashville.
+                </Card.Description>
+              </Card.Content>
+            </Card>
+          </Grid.Column>
+          <Grid.Column>
+            <Card fluid>
+              <Image src={Bild} />
+              <Card.Content>
+                <Card.Header>Daniel</Card.Header>
+                <Card.Meta>Joined in 2016</Card.Meta>
+                <Card.Description>
+                  Daniel is a comedian living in Nashville.
+                </Card.Description>
+              </Card.Content>
+            </Card>
+          </Grid.Column>
+          <Grid.Column>
+            <Card fluid>
+              <Image src={Bild} />
+              <Card.Content>
+                <Card.Header>Daniel</Card.Header>
+                <Card.Meta>Joined in 2016</Card.Meta>
+                <Card.Description>
+                  Daniel is a comedian living in Nashville.
+                </Card.Description>
+              </Card.Content>
+            </Card>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     </React.Fragment>
   );
 };
